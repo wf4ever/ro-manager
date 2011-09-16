8da2570dfe1cacb8d9b2652628a7063c43fe018e
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2011-09-16 11:00:42 +0200

    Use os.walk for scanning directories

--- src/sync/BackgroundSync.py
+++ src/sync/BackgroundSync.py
@@ -7,8 +7,8 @@ Created on 15-09-2011
 from SyncRegistry import SyncRegistry
 import mimetypes
 import logging
-from os.path import isdir, exists
-from os import listdir
+from os.path import isdir, exists, join
+from os import listdir, walk
 
 log = logging.getLogger(__name__)
 
@@ -22,18 +22,18 @@ class BackgroundResourceSync(object):
         '''
         Constructor
         '''
-        self.__rosrsSync = rosrsSync
-        self.__syncRegistries = dict()
+        self.rosrsSync = rosrsSync
+        self.syncRegistries = dict()
         mimetypes.init()
         
     def syncAllResourcesInWorkspace(self, srcDirectory):
         sentFiles = set()
         deletedFiles = set()
         for ro in listdir(srcDirectory):
-            roDirectory = srcDirectory + "/" + ro
+            roDirectory = join(srcDirectory, ro)
             if isdir(roDirectory):
                 for ver in listdir(roDirectory):
-                    verDirectory = roDirectory + "/" + ver
+                    verDirectory = join(roDirectory, ver)
                     if isdir(verDirectory):
                         (s, d) = self.syncAllResources(ro, ver, verDirectory)
                         sentFiles = sentFiles.union(s)
@@ -45,25 +45,17 @@ class BackgroundResourceSync(object):
         return (sentFiles, deletedFiles)
         
     def syncAllResources(self, roId, versionId, srcDirectory):
-        sentFiles = self.__scanDirectories4Put(roId, versionId, srcDirectory, srcDirectory)
+        sentFiles = self.__scanDirectories4Put(roId, versionId, srcDirectory)
         deletedFiles = self.__scanRegistries4Delete(roId, versionId, srcDirectory)
         return (sentFiles, deletedFiles)
     
-    def __scanDirectories4Put(self, roId, versionId, versiondir, srcdir):
-        """
-        Scan all sub-directories in a given source directory.
-        Exceptions are thrown back to the calling program.
-        """
-        directoryList = listdir(srcdir)
+    def __scanDirectories4Put(self, roId, versionId, srcdir):
         sentFiles = set()
-        for directoryComponent in directoryList:
-            path = srcdir + "/" + directoryComponent
-            if isdir(path):
-                log.debug("Adding Directory %s " % (path))
-                sentFiles = sentFiles.union(self.__scanDirectories4Put(roId, versionId, versiondir, path))
-            else:
-                if (self.__checkFile4Put(roId, versionId, versiondir, path)):
-                    sentFiles.add(path)
+        for root, dirs, files in walk(srcdir):
+            for f in files:
+                filepath = join(root, f)
+                if (self.__checkFile4Put(roId, versionId, srcdir, filepath)):
+                    sentFiles.add(filepath)
         return sentFiles
     
     def __checkFile4Put(self, roId, versionId, versiondir, filepath):
@@ -72,27 +64,27 @@ class BackgroundResourceSync(object):
         if rosrsFilepath == "manifest.rdf":
             return
         put = True
-        if (filepath in self.__syncRegistries):
-            put = self.__syncRegistries[filepath].hasBeenModified()
+        if (filepath in self.syncRegistries):
+            put = self.syncRegistries[filepath].hasBeenModified()
         else:
-            self.__syncRegistries[filepath] = SyncRegistry(filepath)
+            self.syncRegistries[filepath] = SyncRegistry(filepath)
         if put:
             contentType = mimetypes.guess_type(filepath)[0]
             fileObject = open(filepath)
             log.debug("Put file %s" % filepath)
-            self.__rosrsSync.putFile(roId, versionId, rosrsFilepath, contentType, fileObject)
+            self.rosrsSync.putFile(roId, versionId, rosrsFilepath, contentType, fileObject)
         return put
     
     def __scanRegistries4Delete(self, roId, versionId, srcdir):
         deletedFiles = set()
-        for r in self.__syncRegistries.viewvalues():
+        for r in self.syncRegistries.viewvalues():
             if r.filename.startswith(srcdir):
                 if not exists(r.filename):
                     log.debug("Delete file %s" % r.filename)
                     deletedFiles.add(r.filename)
                     rosrsFilepath = r.filename[len(srcdir) + 1:]
-                    self.__rosrsSync.deleteFile(roId, versionId, rosrsFilepath)
+                    self.rosrsSync.deleteFile(roId, versionId, rosrsFilepath)
         for f in deletedFiles:
-            self.__syncRegistries[f] = None
+            self.syncRegistries[f] = None
         return deletedFiles
         
