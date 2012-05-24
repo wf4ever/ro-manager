c041af7d8739e1cb8723876125f90c524dbb6f15
Graham Klyne <gk-github@ninebynine.org>
2012-05-24 10:03:48 +0100

    Sample and test code added

---
+++ src/roweb/sample-asq.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+HOST=http://andros.zoo.ox.ac.uk:8080    # Service endpoint URI
+
+# Retrieve service description and extract template
+
+echo "==== Retrieve URI template for evaluate checklist ===="
+TEMPLATE=`asq -r "http://andros.zoo.ox.ac.uk:8080/" -f "%(t)s" "SELECT ?t WHERE { ?s <http://purl.org/ro/service/evaluate/checklist> ?t }"`
+echo "==== Template: <$TEMPLATE>"
+
+# URI template expansion
+echo "==== Request URI-template expansion ===="
+cat >sample-params.txt <<END
+{
+  "template": "$TEMPLATE",
+  "params":
+  {
+    "RO": "http://andros.zoo.ox.ac.uk/workspace/wf4ever-ro-catalogue/v0.1/simple-requirements/",
+    "minim": "simple-requirements-minim.rdf",
+    "purpose": "Runnable"
+  }
+}
+END
+
+EVALURI=$HOST`curl -X POST --data @sample-params.txt $HOST/uritemplate`
+
+echo "==== URI: $EVALURI"
+
+# Evaluation results with URI parameters:
+
+echo "==== Request evaluation result with parameters, as RDF/Turtle ===="
+curl -H "accept: text/turtle" $EVALURI
+
+echo "==== Request evaluation result with parameters, as RDF/XML ===="
+curl -H "accept: application/rdf+xml" $EVALURI
+
+# End.
