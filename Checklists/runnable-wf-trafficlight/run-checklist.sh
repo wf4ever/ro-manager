0d859c423ae4d54e5c8074f291499c369beda92d
Graham Klyne <gk-github@ninebynine.org>
2012-11-01 15:25:56 +0000

    Add checklist evaluation display mockup and scripts to run checklist

--- Checklists/runnable-wf-trafficlight/run-checklist.sh
+++ Checklists/runnable-wf-trafficlight/run-checklist.sh
@@ -3,11 +3,57 @@
 # RO manager checklist sample script
 #
 
-ROURI="<RO>"        # URI of RO to evaluate
-RESURI="."          # URI of target resource (relative to RO)
+# URI if minim file (needs to be absolute URI)
+MINIMURI="file://`pwd -P`/checklist.rdf"
 
-ro evaluate checklist --debug -v -d $ROURI -a simple-requirements-minim.rdf "Runnable" $RESURI
+# URI of RO to evaluate
+ROURI="http://sandbox.wf4ever-project.org/rodl/ROs/simple-requirements/"
 
-#echo "--------"
+# URI of target resource (relative to RO)
+RESURI="."
+
+echo "--------"
+
+#ro evaluate checklist --debug -v -d $ROURI -a $MINIMURI "Runnable" $RESURI
+echo "ro evaluate checklist -d $ROURI -a  $MINIMURI "Runnable" $RESURI"
+ro evaluate checklist -d $ROURI -a  $MINIMURI "Runnable" $RESURI
+
+echo "--------"
+
+# URI of RO to evaluate
+ROURI="http://sandbox.wf4ever-project.org/rodl/ROs/wf74ph/"
+
+# URI of target resource (relative to RO)
+RESURI="."
+
+#ro evaluate checklist --debug -v -d $ROURI -a $MINIMURI "Runnable" $RESURI
+echo "ro evaluate checklist -d $ROURI -a  $MINIMURI "Runnable" $RESURI"
+ro evaluate checklist -d $ROURI -a  $MINIMURI "Runnable" $RESURI
+
+echo "--------"
+
+# URI of RO to evaluate
+ROURI="http://sandbox.wf4ever-project.org/rodl/ROs/wf74-repeat-fail1/"
+
+# URI of target resource (relative to RO)
+RESURI="."
+
+#ro evaluate checklist --debug -v -d $ROURI -a $MINIMURI "Runnable" $RESURI
+echo "ro evaluate checklist -d $ROURI -a  $MINIMURI "Runnable" $RESURI"
+ro evaluate checklist -d $ROURI -a  $MINIMURI "Runnable" $RESURI
+
+echo "--------"
+
+# URI of RO to evaluate
+ROURI="http://sandbox.wf4ever-project.org/rodl/ROs/wf74-repeat-fail2/"
+
+# URI of target resource (relative to RO)
+RESURI="."
+
+#ro evaluate checklist --debug -v -d $ROURI -a $MINIMURI "Runnable" $RESURI
+echo "ro evaluate checklist -d $ROURI -a  $MINIMURI "Runnable" $RESURI"
+ro evaluate checklist -d $ROURI -a  $MINIMURI "Runnable" $RESURI
+
+echo "--------"
 
 # End.
