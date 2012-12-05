7423545c13e683efdb6c8f504b9939649406f82e
Graham Klyne <gk-github@ninebynine.org>
2012-12-05 17:08:27 +0000

    New scripts to run checklist evaluation on local RO

---
+++ Checklists/runnable-wf-trafficlight/ro-config-local.sh
@@ -0,0 +1,12 @@
+ROPATH="/usr/workspace/wf4ever-ro-catalogue/v0.1"
+ROSRS_URI="http://sandbox.wf4ever-project.org/rodl/ROs/"
+ROSRS_ACCESS_TOKEN="32801fc0-1df1-4e34-b"
+RO=ro
+
+$RO config -v \
+  -b $ROPATH \
+  -r $ROSRS_URI \
+  -t "$ROSRS_ACCESS_TOKEN" \
+  -n "Test user" \
+  -e "testuser@example.org"
+
