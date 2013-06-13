546ca016218ddaad32110d0d8025a9ca9404edbd
Graham Klyne <gk-github@ninebynine.org>
2013-06-13 14:41:24 +0100

    Update summary of changes made

--- src/rocommand/__init__.py
+++ src/rocommand/__init__.py
@@ -22,4 +22,7 @@
                              # Ordering of checklist display
                              # Various display formatting enhancements
 #__version__ = "0.2.11"      # Add RO evolution commands; ro snapshow, ro archive, ro freeze
-__version__ = "0.2.12"      # ... (Fix queries to work with rdflib-sparql)
+__version__ = "0.2.12"      # Fix queries to work with rdflib 4.0.1 and rdflib-sparql
+                            # Fix some bugs in RODL synchronization and evolution
+                            # Refactor Minim model and add some new capabilities
+
