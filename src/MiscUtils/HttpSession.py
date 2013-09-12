7177ffd29fe34fe21ec223c48b2c8e8213bf204d
Graham Klyne <gk-github@ninebynine.org>
2013-09-12 12:10:24 +0100

    Revise HTTP_Session to catch and log exceptions when accessing HTTP response

--- src/MiscUtils/HttpSession.py
+++ src/MiscUtils/HttpSession.py
@@ -257,7 +257,7 @@ class HTTP_Session(object):
             log.debug("HTTP_Session.doRequest response:   "+str(status)+" "+reason)
             log.debug("HTTP_Session.doRequest rspheaders: "+repr(headers))
         except Exception, e:
-            log.warn("HTTP_Session error %r accessing %s"%(e, uripath))
+            log.warn("HTTP_Session error %r accessing %s with request headers %r"%(e, uripath, reqheaders))
             status = 900
             reason = str(e)
             headers = {"_headerlist": []}
