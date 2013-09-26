8b92d30f1cb92cf122fa1cc375a6cd85d39cf002
Graham Klyne <gk-github@ninebynine.org>
2013-09-26 07:53:39 +0100

    Update change log for version 0.2.17

--- src/rocommand/__init__.py
+++ src/rocommand/__init__.py
@@ -51,4 +51,13 @@
                              # Modify HTTP doRequestr methiods top return URI as string, not rdflib.URIRef
                              # Add retry logic to make Overlay ROs behave more consistently
                              # Minor documentation and script tweaks
-__version__ = "0.2.17"      # ...
+__version__ = "0.2.17"      # Updates to RODL inbterface synchronization.  Includes option to push ZIP file bundle
+                             # Provide label for checklist target resource as well as the RO
+                             # Overlay RO service documentation updated
+                             # Refactoring of HTTP access to ROs (HTTP_Session), and logic to catch and log HTTP connection errors
+                             # Fix bug in RO metadata access when nested ROs are used
+                             # Rename minim:testedConstraint to minim:testedChecklist in checklist results vocabulary
+                             # Fix bug in handling of checklist "miss" option. 
+                             # Improve diagnostics/logging in several areas, especially the checklist service
+                             # Fix listening port error in Overlay RO service
+                             # Improve handling of errors when parsing RO annotations
