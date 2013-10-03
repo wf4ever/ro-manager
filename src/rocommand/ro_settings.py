c6d4ca5663922460b8238dd38f568a17f61fde64
Graham Klyne <gk-github@ninebynine.org>
2013-10-03 16:58:58 +0100

    Initial cut of utility to create RO for benchmarking

--- src/rocommand/ro_settings.py
+++ src/rocommand/ro_settings.py
@@ -7,10 +7,12 @@ RO manager parameter settings
 import __init__
 VERSION_NUM   = __init__.__version__
 
-RO_VERSION    = "v"+VERSION_NUM
-VERSION       = RO_VERSION+" (RODL)"
-MANIFEST_DIR  = ".ro"
-MANIFEST_FILE = "manifest.rdf"
+RO_VERSION      = "v"+VERSION_NUM
+VERSION         = RO_VERSION+" (RODL)"
+MANIFEST_DIR    = ".ro"
+MANIFEST_FILE   = "manifest.rdf"
+MANIFEST_FORMAT = "application/rdf+xml"
+MANIFEST_REF    = MANIFEST_DIR + "/" + MANIFEST_FILE
 REGISTRIES_FILE = ".registries.json"
 
 # End.
