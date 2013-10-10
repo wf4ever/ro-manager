wf4ever/ro-manager

commit 3ea50a13eb960d38079da53a13809193fff16f2f

Adjust iaeval package to work with PyPI copy of uritemplate

src/iaeval/ro_eval_minim.py
@@ -19,7 +19,10 @@ import rdflib
 #import rdflib.namespace
 #from rdflib import URIRef, Namespace, BNode
 #from rdflib import Literal
-from uritemplate import uritemplate
+try:
+    from uritemplate import uritemplate
+except ImportError:
+    import uritemplate
 
 from rocommand.ro_uriutils   import isLiveUri, resolveUri
 from rocommand.ro_namespaces import RDF, RDFS, ORE, DCTERMS
@@ -297,7 +300,7 @@ def evalContentMatch(rometa, rule, constraintbinding):
             if template:
                 # Construct URI for file from template
                 # Uses code copied from http://code.google.com/p/uri-templates
-                fileref = uritemplate.expand(template, simplebinding)
+                fileref = g.expand(template, simplebinding)
                 fileuri = rometa.getComponentUri(fileref)
                 # Test if URI is aggregated
                 log.debug("evalContentMatch RO aggregates %s (%s)"%(fileref, str(fileuri)))

src/iaeval/ro_minim.py
@@ -13,7 +13,10 @@ log = logging.getLogger(__name__)
 
 import rdflib
 import rdflib.namespace
-from uritemplate import uritemplate
+try:
+    from uritemplate import uritemplate
+except ImportError:
+    import uritemplate
 
 from rocommand import ro_manifest
 from rocommand import ro_namespaces

commit e87c7bf305f479e8f0f5421299e79eabc9c01e86

Fix stupid error introduced in commit 3ea50a1

src/iaeval/ro_eval_minim.py
@@ -300,7 +300,7 @@ def evalContentMatch(rometa, rule, constraintbinding):
             if template:
                 # Construct URI for file from template
                 # Uses code copied from http://code.google.com/p/uri-templates
-                fileref = g.expand(template, simplebinding)
+                fileref = uritemplate.expand(template, simplebinding)
                 fileuri = rometa.getComponentUri(fileref)
                 # Test if URI is aggregated
                 log.debug("evalContentMatch RO aggregates %s (%s)"%(fileref, str(fileuri)))